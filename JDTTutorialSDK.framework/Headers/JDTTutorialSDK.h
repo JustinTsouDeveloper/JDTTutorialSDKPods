decbdca808a0936b49f90a1d4880691eac68b0bc
Justin Tsou <killkill8250@gmail.com>
2021-03-02 15:23:28 +0800

    Add JDTTutorialSDK framework file.

---
+++ JDTTutorialSDK.framework/Headers/JDTTutorialSDK.h
@@ -0,0 +1,18 @@
+//
+//  JDTTutorialSDK.h
+//  JDTTutorialSDK
+//
+//  Created by JustinTsou on 2021/2/26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JDTTutorialSDK.
+FOUNDATION_EXPORT double JDTTutorialSDKVersionNumber;
+
+//! Project version string for JDTTutorialSDK.
+FOUNDATION_EXPORT const unsigned char JDTTutorialSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JDTTutorialSDK/PublicHeader.h>
+
+
